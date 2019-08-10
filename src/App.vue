e7489f05d05b1ecdc67c998db057d547df3f32a5
bchutchison <benhutchison85@gmail.com>
2019-08-10 12:17:45 +0100

    Dropdown select complete

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template lang="html">
-
+  <article-select :articles="articles"></article-select>
 </template>
 
 <script>
+import { eventBus } from '@/main.js';
+import ArticleSelect from '@/components/ArticleSelect.vue';
+// import ArticleSelect from '@/components/ArticleDetail.vue';
 
 export default {
+  components: {
+    'article-select': ArticleSelect
+  },
   data() {
     return {
-      articles: []
+      articles: [],
+      selectedArticl: null
     };
   },
   mounted() {
+    eventBus.$on('article-selected', (selectedIndex) => {
+      this.selectedArticle = this.articles[selectedIndex]; 
+    });
     fetch('https://content.guardianapis.com/search?section=film&api-key=test')
       .then(res => res.json())
       .then(articles => this.articles = articles.response.results);
