d1d7ae370dbb4d45e79b246733c3de184c077e02
bchutchison <benhutchison85@gmail.com>
2019-08-10 13:37:18 +0100

    Article detail

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template lang="html">
-  <article-select :articles="articles"></article-select>
+  <div>
+    <article-select :articles="articles"></article-select>
+    <article-detail :article="selectedArticle"></article-detail>
+  </div>
 </template>
 
 <script>
 import { eventBus } from '@/main.js';
 import ArticleSelect from '@/components/ArticleSelect.vue';
-// import ArticleSelect from '@/components/ArticleDetail.vue';
+import ArticleDetail from '@/components/ArticleDetail.vue';
 
 export default {
   components: {
-    'article-select': ArticleSelect
+    'article-select': ArticleSelect,
+    'article-detail': ArticleDetail
   },
   data() {
     return {
       articles: [],
-      selectedArticl: null
+      selectedArticle: null
     };
   },
   mounted() {
     eventBus.$on('article-selected', (selectedIndex) => {
-      this.selectedArticle = this.articles[selectedIndex]; 
+      this.selectedArticle = this.articles[selectedIndex];
     });
     fetch('https://content.guardianapis.com/search?section=film&api-key=test')
       .then(res => res.json())
---
+++ src/components/ArticleDetail.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <ul v-if="article !== null">
+    <li><span class="bold">Title: </span>{{ article.webTitle}}</li>
+    <li><span class="bold">Section Name: </span>{{article.sectionName}}</li>
+    <li><span class="bold">Publication Date: </span>{{article.webPublicationDate.toLocaleString().replace('Z', '').replace('T', ' ') }}</li>
+    <li><span class="bold">Link: </span>{{article.webUrl}}</li>
+
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'article-detail',
+  props: ['article']
+}
+</script>
+
+<style lang="css" scoped
+.bold {
+  font-weight: bold;
+}
+</style>
