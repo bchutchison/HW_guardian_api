7a5e7859b733703c4f95e60fd3446613237a62db
bchutchison <benhutchison85@gmail.com>
2019-08-10 11:26:30 +0100

    created link to api dataset

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,18 @@
 </template>
 
 <script>
+
 export default {
+  data() {
+    return {
+      articles: []
+    };
+  },
+  mounted() {
+    fetch('https://content.guardianapis.com/search?section=film&api-key=test')
+      .then(res => res.json())
+      .then(articles => this.articles = articles.response.results);
+  }
 }
 </script>
 
