1d62bb27b5357ee3fa47c45eb552ab6397115d00
bchutchison <benhutchison85@gmail.com>
2019-08-10 13:46:46 +0100

    film link added

--- src/components/ArticleDetail.vue
+++ src/components/ArticleDetail.vue
@@ -1,11 +1,12 @@
 <template lang="html">
-  <ul v-if="article !== null">
-    <li><span class="bold">Title: </span>{{ article.webTitle}}</li>
-    <li><span class="bold">Section Name: </span>{{article.sectionName}}</li>
-    <li><span class="bold">Publication Date: </span>{{article.webPublicationDate.toLocaleString().replace('Z', '').replace('T', ' ') }}</li>
-    <li><span class="bold">Link: </span>{{article.webUrl}}</li>
-
-  </ul>
+  <div class="article-detail">
+    <ul v-if="article !== null">
+      <li><span class="bold">Title: </span>{{ article.webTitle}}</li>
+      <li><span class="bold">Section Name: </span>{{article.sectionName}}</li>
+      <li><span class="bold">Publication Date: </span>{{article.webPublicationDate.toLocaleString().replace('Z', '').replace('T', ' ') }}</li>
+      <li><span class="bold">Link: </span><a href="article.webUrl">{{article.webUrl}}</a></li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -15,7 +16,12 @@ export default {
 }
 </script>
 
-<style lang="css" scoped
+<style lang="css" scoped>
+.article-detail {
+  font-family: "Helvetica";
+  letter-spacing: 0.08em;
+}
+
 .bold {
   font-weight: bold;
 }
